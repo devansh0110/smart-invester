b5cc06d5a49e3002d57f579de2e783ec572b8a45
devansh0110 <devansh0110@gmail.com>
2024-09-28 11:56:32 +0530

    Updated CORS policy

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: '*', // Allow all origins
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    }),
+  );
   // Enable validation globally
   app.useGlobalPipes(
     new ValidationPipe({
